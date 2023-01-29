4101fa1759f8d5e28d33330370eac2b4a0212641
Maxim Shirokov <maximshirokov21@gmail.com>
2023-01-29 16:16:43 +0500

    Add readline-sync lib, cli module and function for message of welcome

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,5 @@
 #!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
+import { welcomeMessage } from "../src/cli.js";
+
+welcomeMessage();
---
+++ src/cli.js
@@ -0,0 +1,11 @@
+import readlineSync from 'readline-sync';
+
+function welcomeMessage() {
+    console.log('Welcome to the Brain Games!');
+
+    const name = readlineSync.question('May I have your name? ');
+
+    console.log(`Hello, ${name}!`);
+}
+
+export { welcomeMessage };
